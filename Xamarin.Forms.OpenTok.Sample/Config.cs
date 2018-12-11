f1710b030c7ba59c00b455aeb9b56e860296dcef
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-12-12 01:54:54 +0300

    Add comemnt

--- Xamarin.Forms.OpenTok.Sample/Config.cs
+++ Xamarin.Forms.OpenTok.Sample/Config.cs
@@ -3,6 +3,6 @@ namespace Xamarin.Forms.OpenTok.Sample
 {
     public static class Config
     {
-        public static string KeysUrl = "";
+        public static string KeysUrl = ""; // URL for retrieving keys (or you may hardcode them, check App.cs)
     }
 }
